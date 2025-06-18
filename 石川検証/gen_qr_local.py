6c09f9333ea85c53e1080eeaa5f1a78cbf5656c4
nt12chin <chihiro.2801@gmail.com>
2025-06-18 13:00:17 +0900

    Delete 石川検証 directory

--- 石川検証/gen_qr_local.py
+++
@@ -1,8 +0,0 @@
-import qrcode
-
-# ローカルサーバーのURL（例: http://192.168.100.70:8000）
-url = 'http://192.168.100.70:8000'
-
-img = qrcode.make(url)
-img.save('qrcode_local.png')
-print('QRコード画像を保存しました: qrcode_local.png')
